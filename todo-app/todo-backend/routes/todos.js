243837055390f2d2db66db893f692b823ee8213a
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2022-04-05 03:15:48 +0300

    Ex. 12.7 done

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { Todo } = require('../mongo')
+const { Todo } = require('../mongo');
 const router = express.Router();
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
+  const todos = await Todo.find({});
   res.send(todos);
 });
 
@@ -13,37 +13,84 @@ router.post('/', async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
     done: false
-  })
+  });
   res.send(todo);
 });
 
 const singleRouter = express.Router();
 
+const reId = /^[0-9A-Fa-f]{24}$/;
+
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
+  if (!id || typeof(id) !== "string") {
+    return res.sendStatus(400);
+  }
 
-  next()
+  const trimmedId = id.trim();
+  if (!reId.test(trimmedId)) {
+    return res.sendStatus(400);
+  }
+
+  req.todo = await Todo.findById(trimmedId);
+  if (!req.todo) return res.sendStatus(404);
+
+  next();
 }
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete();
   res.sendStatus(200);
 });
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  if (!req.todo) {
+    return res.sendStatus(404);
+  }
+
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  if (!req.todo) {
+    return res.sendStatus(404);
+  }
+
+  const entryToUpdate = {}
+
+  const {text, done} = req.body;
+
+  if (text || typeof(text) === "string") {
+    const trimmedText = text.trim();
+    if (trimmedText.length > 0) {
+      entryToUpdate.text = trimmedText;
+    }
+    else {
+      return res.sendStatus(400);
+    }
+  }
+
+  if (typeof(done) === "boolean") {
+    entryToUpdate.done = done;
+  }
+  else {
+    return res.sendStatus(400);
+  }
+
+  const updatedEntry =
+    await Todo.findByIdAndUpdate(
+      req.todo._id, entryToUpdate, {
+        new: true,
+        useFindAndModify: false,
+      });
+
+  res.json(updatedEntry);
 });
 
-router.use('/:id', findByIdMiddleware, singleRouter)
+router.use('/:id', findByIdMiddleware, singleRouter);
 
 
 module.exports = router;
